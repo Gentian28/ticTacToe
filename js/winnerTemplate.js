338f6da44365a9c2b826d0d57bbd466b543c1891
Gentian Shkembi <gentian.shkembi@gmail.com>
2019-12-23 10:51:21 +0100

    Handle draw

--- js/winnerTemplate.js
+++ js/winnerTemplate.js
@@ -1,19 +1,34 @@
 export class WinnerTemplate {
     playerOneTemplate = `
-    <div class="x-symbol">
-        <div class="left"></div>
-        <div class="right"></div>
-    </div>`;
+        <div class="x-symbol">
+            <div class="left"></div>
+            <div class="right"></div>
+        </div>
+    `;
 
     playerTwoTemplate = `
-    <svg class="circle-symbol" width="50px" height="50px">
-    <circle class="path" stroke="#F2EBD3" stroke-width="6" stroke-miterlimit="10" cx="25" cy="25" r="22"
-        stroke="#F2EBD3" fill="transparent" />
-    </svg>`;
+        <svg class="circle-symbol" width="50px" height="50px">
+            <circle class="path" stroke="#F2EBD3" stroke-width="6" stroke-miterlimit="10" cx="25" cy="25" r="22"
+                stroke="#F2EBD3" fill="transparent" />
+        </svg>
+    `;
+
+    drawTemplate = `
+        <div id="draw-template">
+            <div class="x-symbol">
+                <div class="left"></div>
+                <div class="right"></div>
+            </div>
+            <svg class="circle-symbol" width="50px" height="50px">
+                <circle class="path" stroke="#F2EBD3" stroke-width="6" stroke-miterlimit="10" cx="25" cy="25" r="22"
+                    stroke="#F2EBD3" fill="transparent" />
+            </svg>
+        </div>
+    `;
 
     renderWinner(playerTemplate) {
         const template = `
-        <div id="winner">
+        <div id="result">
             ${playerTemplate}
             <div>winner!</div>
             <button type="button" id="restartGame">Start new game</button>
@@ -21,7 +36,18 @@ export class WinnerTemplate {
         return template;
     }
 
+    handleDraw(playerTemplate) {
+        const template = `
+        <div id="result">
+            ${playerTemplate}
+            <div>draw!</div>
+            <button type="button" id="restartGame">Start new game</button>
+        </div>`;
+        return template;
+    }
+
     checkWinner(turn, moves) {
+        let winner = false;
         // if (ticTacToe.moves[0] === 'X' && ticTacToe.moves[1] === 'X' && ticTacToe.moves[2] === 'X') {
         //     document.getElementById('line-0-1-2').style.width = '100%';
         // }
@@ -47,6 +73,12 @@ export class WinnerTemplate {
             )
         ) {
             message.innerHTML = turn ? this.renderWinner(this.playerTwoTemplate) : this.renderWinner(this.playerOneTemplate);
+            winner = true;
+            ticTacToeField.style.transform = 'scale(0)';
+            restartGame.addEventListener('click', this.triggerReset);
+        }
+        if (!moves.includes(null) && !winner) {
+            message.innerHTML = this.handleDraw(this.drawTemplate);
             ticTacToeField.style.transform = 'scale(0)';
             restartGame.addEventListener('click', this.triggerReset)
         }
